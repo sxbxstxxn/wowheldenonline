46192d94a47a0ce6de3294f0e2dde07a574cdf0d
root <root@server.sebastian-christoph.de>
2019-09-06 15:58:40 +0200

    server selection

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -13,7 +13,33 @@ class DefaultController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('index.html.twig');
+        $server['regions'] = [
+            'america' => 'America',
+            'europe' => 'Europe',
+            'oceanic' => 'Oceanic'
+        ];
+        $server['types'] = [
+            'pve' => 'PVE',
+            'pvp' => 'PVP',
+            'rp' => 'RP'
+        ];
+
+        $server['names']['america']['pve'] = ['Ashkandi', 'Atiesh', 'Azuresong', 'Mankrik', 'Myzrael', 'Old Blanchy', 'Pagle', 'Westfall', 'Windseeker'];
+        $server['names']['america']['pvp'] = ['Anathema', 'Bigglesworth', 'Benediction', 'Blaumeux', 'Faerlina', 'Fairbanks', 'Herod', 'Incendius', 'Kirtonos', 'Kurinaxx', 'Kromcrush', 'Netherwind', 'Rattlegore', 'Skeram', 'Smolderweb', 'Stalagg', 'Sulfuras', 'Thalnos', 'Thunderfury', 'Whitemane'];
+        $server['names']['america']['rp'] = ['Bloodsail Buccaneers', 'Deviant Delight', 'Grobbulus'];
+
+        $server['names']['europe']['pve'] = ['Mirage Raceway', 'Nethergarde Keep', 'Pyrewood Village', 'Auberdine', 'Everlook', 'Razorfen', 'Lakeshire', 'Chromie'];
+        $server['names']['europe']['pvp'] = ['Ashbringer', 'Bloodfang', 'Dreadmist', 'Firemaw', 'Flamelash', 'Gandling', 'Gehennas', 'Golemagg', 'Judgement', 'Mograine', 'Noggenfogger', 'Razorgore', 'Shazzrah', 'Skullflame', 'Stonespine', 'Ten Storms', 'Amnennar', 'Sulfuron', 'Finkle', 'Lucifron', 'Venoxis','Patchwerk','Dragons Call', 'Transcendence', 'Flamegor', 'Wyrmthalak','Rhokdelar'];
+        $server['names']['europe']['rp'] = ['Hydraxian Waterlords', 'Zandalar Tribe'];
+
+        $server['names']['oceanic']['pve'] = ['Remulos'];
+        $server['names']['oceanic']['pvp'] = ['Arugal', 'Felstriker', 'Yojamba'];
+        $server['names']['oceanic']['rp'] = ['test'];
+
+        return $this->render(
+            'index.html.twig',
+            ['server' => $server]
+            );
     }
 
     /**
